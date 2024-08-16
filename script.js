cb6aff791a7127397fbc4141d115272317be3de4
dakota-bayer <89050126+dakota-bayer@users.noreply.github.com>
2024-08-16 12:15:57 -0500

    Add js layout. Ready to add roman numeral conversion

--- script.js
+++ script.js
@@ -2,15 +2,63 @@ const input = document.getElementById('number');
 const button = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
+const conversionArray = [
+    {
+        value: 1000,
+        character: "M"
+    },
+    {
+        value: 500,
+        character: "D"
+    },
+    {
+        value: 100,
+        character: "C"
+    },
+    {
+        value: 50,
+        character: "L"
+    },
+    {
+        value: 10,
+        character: "X"
+    },
+    {
+        value: 5,
+        character: "V"
+    },
+    {
+        value: 1,
+        character: "I"
+    },
+];
+
 button.addEventListener('click', () => {
     // Check input
+    const isValid = IsValidInput();
+    if(!isValid){
+        window.alert("not valid");
+        return;
+    }
 
     // Parse input to int
-
+    const number = parseInt(input.value);
+    
     // Convert to roman numeral string
-
+    const romanNumeral = "roman numeral"
     // Display roman numeral string
+    output.innerText = romanNumeral;
 
-    console.log('button clicked');
-    output.innerText = 'button clicked';
-})
+    console.log(JSON.stringify(conversionArray));
+});
+
+function IsValidInput(){
+    const inputText = input.value;
+    const parsedInput = parseInt(inputText);
+
+    if(!input.value || isNaN(parsedInput) || parsedInput < 0){
+        return false;
+    }
+
+    return true;
+}
