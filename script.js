fe5bcb1f5c432034c9ebc8dc517cad4976935672
dakota-bayer <89050126+dakota-bayer@users.noreply.github.com>
2024-08-16 07:09:58 -0500

    Added event listener and styling.

---
+++ script.js
@@ -0,0 +1,16 @@
+const input = document.getElementById('number');
+const button = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+button.addEventListener('click', () => {
+    // Check input
+
+    // Parse input to int
+
+    // Convert to roman numeral string
+
+    // Display roman numeral string
+
+    console.log('button clicked');
+    output.innerText = 'button clicked';
+})
